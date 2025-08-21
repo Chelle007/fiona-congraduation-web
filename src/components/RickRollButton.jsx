59c0df26459a1645f0a264c4c55f062cb3901748
desraymondz <desraymondz@gmail.com>
2025-08-22 02:59:48 +0800

    open rick roll link in a new page

--- src/components/RickRollButton.jsx
+++ src/components/RickRollButton.jsx
@@ -3,7 +3,7 @@ import { RainbowButton } from "@/components/magicui/rainbow-button";
 
 export function RickRollButton() {
     const handleClick = () => {
-        window.location.href = "https://youtu.be/iik25wqIuFo";
+        window.open("https://youtu.be/iik25wqIuFo", "_blank");
     };
 
     return (
