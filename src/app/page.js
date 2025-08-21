31a9b371a2ad2febdba4159ab2824cd935cfdc24
desraymondz <desraymondz@gmail.com>
2025-08-22 01:24:15 +0800

    add confetti on scroll

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { TextReveal } from "@/components/magicui/text-reveal";
+import ConfettiOnScroll from "@/components/ConfettiOnScroll";
 import { AnimatedListDemo } from "@/components/AnimatedListDemo";
 import { Iphone15ProDemo } from "@/components/Iphone15ProDemo";
 
@@ -11,6 +12,7 @@ export default function Page() {
       <TextReveal>
         22nd Aug 2025
       </TextReveal>
+      <ConfettiOnScroll />
 
       <Iphone15ProDemo size="w-100">
         <AnimatedListDemo />
